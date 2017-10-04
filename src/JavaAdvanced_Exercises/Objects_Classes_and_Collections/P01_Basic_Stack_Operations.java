8402535443a3dc15e9eb4c07d322a692c8f564a6
stanislavkisyov <stanislav.kisiov@gmail.com>
2017-10-04 11:25:49 +0300

    new

---
+++ src/JavaAdvanced_Exercises/Objects_Classes_and_Collections/P01_Basic_Stack_Operations.java
@@ -0,0 +1,9 @@
+package JavaAdvanced_Exercises.Objects_Classes_and_Collections;
+
+import java.util.Scanner;
+
+public class P01_Basic_Stack_Operations {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
