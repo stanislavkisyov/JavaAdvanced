1abcbb5fff2147609b24029e89785c2432610b88
stanislavkisyov <stanislav.kisiov@gmail.com>
2017-10-04 14:40:27 +0300

    Truck Tour

--- src/JavaAdvanced_Exercises/Objects_Classes_and_Collections/P03_Basic_Queue_Operations.java
+++ src/JavaAdvanced_Exercises/Objects_Classes_and_Collections/P03_Basic_Queue_Operations.java
@@ -1,4 +1,41 @@
 package JavaAdvanced_Exercises.Objects_Classes_and_Collections;
 
+import javax.naming.InsufficientResourcesException;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
+
 public class P03_Basic_Queue_Operations {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] commands = scanner.nextLine().split("\\s+");
+        int numbersOfElements = Integer.parseInt(commands[0]);
+        int removeElements = Integer.parseInt(commands[1]);
+        int peekElements = Integer.parseInt(commands[2]);
+        String[] number = scanner.nextLine().split("\\s+");
+        ArrayDeque<Integer> queue = new ArrayDeque<>();
+
+        int minElement = Integer.MAX_VALUE;
+        for (int i = 0; i < numbersOfElements; i++) {
+            queue.add(Integer.parseInt(number[i]));
+        }
+        for (int i = 0; i < removeElements; i++) {
+            queue.remove();
+        }
+
+        if (queue.contains(peekElements)) {
+            System.out.println(true);
+        } else if (queue.isEmpty()) {
+            System.out.println("0");
+        } else {
+            for (Integer integer : queue) {
+                if (integer < minElement) {
+                    minElement = integer;
+                }
+            }
+            System.out.println(minElement);
+        }
+    }
 }
+
---
+++ src/JavaAdvanced_Exercises/Objects_Classes_and_Collections/P04_Truck_Tour.java
@@ -0,0 +1,4 @@
+package JavaAdvanced_Exercises.Objects_Classes_and_Collections;
+
+public class P04_Truck_Tour {
+}
