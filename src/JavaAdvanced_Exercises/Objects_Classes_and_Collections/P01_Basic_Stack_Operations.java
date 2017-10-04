f04b0590c2cda36b50c6571f5c6fa3ee29ee152b
stanislavkisyov <stanislav.kisiov@gmail.com>
2017-10-04 12:12:51 +0300

    Maximum Element

--- src/JavaAdvanced_Exercises/Objects_Classes_and_Collections/P01_Basic_Stack_Operations.java
+++ src/JavaAdvanced_Exercises/Objects_Classes_and_Collections/P01_Basic_Stack_Operations.java
@@ -1,9 +1,41 @@
 package JavaAdvanced_Exercises.Objects_Classes_and_Collections;
 
+import java.util.ArrayDeque;
 import java.util.Scanner;
 
 public class P01_Basic_Stack_Operations {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        String[] commands = scanner.nextLine().split("\\s+");
+
+        int numberOfDigits = Integer.parseInt(commands[0]);
+        int elementsToPop = Integer.parseInt(commands[1]);
+        int elementsContainsInPop = Integer.parseInt(commands[2]);
+        int minElement = Integer.MAX_VALUE;
+        String[] numbers = scanner.nextLine().split("\\s+");
+
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+
+        for (int i = 0; i < numberOfDigits; i++) {
+            stack.push(Integer.parseInt(numbers[i]));
+        }
+
+        for (int i = 0; i < elementsToPop; i++) {
+            stack.pop();
+        }
+        if (stack.contains(elementsContainsInPop)) {
+            System.out.println(true);
+        } else if (stack.size() == 0) {
+            System.out.println("0");
+        } else {
+            for (Integer integer : stack) {
+                if (integer < minElement) {
+                    minElement = integer;
+                }
+            }
+            System.out.println(minElement);
+        }
+
     }
 }
---
+++ src/JavaAdvanced_Exercises/Objects_Classes_and_Collections/P02_Maximum_Element.java
@@ -0,0 +1,4 @@
+package JavaAdvanced_Exercises.Objects_Classes_and_Collections;
+
+public class P02_Maximum_Element {
+}
