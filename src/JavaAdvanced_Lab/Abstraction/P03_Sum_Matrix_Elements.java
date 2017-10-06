a708427a31c38a4c3922057566fdf079308bf4e3
stanislavkisyov <stanislav.kisiov@gmail.com>
2017-10-06 14:34:58 +0300

    Sum Matrix Elements

---
+++ src/JavaAdvanced_Lab/Abstraction/P03_Sum_Matrix_Elements.java
@@ -0,0 +1,9 @@
+package JavaAdvanced_Lab.Abstraction;
+
+import java.util.Scanner;
+
+public class P03_Sum_Matrix_Elements {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
