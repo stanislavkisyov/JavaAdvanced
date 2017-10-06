6f51f2b8db3e23c27a1bdb99a6e536281d1723c5
stanislavkisyov <stanislav.kisiov@gmail.com>
2017-10-06 13:31:46 +0300

    Encrypt Sort and Print Array

--- src/JavaAdvanced_Lab/Abstraction/P01_Calculate_Triangle_Area_Method.java
+++ src/JavaAdvanced_Lab/Abstraction/P01_Calculate_Triangle_Area_Method.java
@@ -1,4 +1,22 @@
 package JavaAdvanced_Lab.Abstraction;
 
+import java.util.Scanner;
+
 public class P01_Calculate_Triangle_Area_Method {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String[] input = scanner.nextLine().split("\\s+");
+
+        double base = Double.parseDouble(input[0]);
+        double height = Double.parseDouble(input[1]);
+        double area = calcTriangleArea(base, height);
+
+        System.out.printf("Area = %.2f", area);
+    }
+
+    private static double calcTriangleArea(double base, double height) {
+        return base * height / 2;
+    }
 }
---
+++ src/JavaAdvanced_Lab/Abstraction/P02_Encrypt_Sort_and_Print_Array.java
@@ -0,0 +1,4 @@
+package JavaAdvanced_Lab.Abstraction;
+
+public class P02_Encrypt_Sort_and_Print_Array {
+}
