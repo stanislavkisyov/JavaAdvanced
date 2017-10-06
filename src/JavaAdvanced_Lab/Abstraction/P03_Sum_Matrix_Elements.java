5df5e34a589e498a0cef37c98036a37df1955f86
stanislavkisyov <stanislav.kisiov@gmail.com>
2017-10-06 15:10:05 +0300

    Maximum Sum of 2x2 Submatrix

--- src/JavaAdvanced_Lab/Abstraction/P03_Sum_Matrix_Elements.java
+++ src/JavaAdvanced_Lab/Abstraction/P03_Sum_Matrix_Elements.java
@@ -5,5 +5,22 @@ import java.util.Scanner;
 public class P03_Sum_Matrix_Elements {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String[] input = scanner.nextLine().split(", ");
+
+        int row1 = Integer.parseInt(input[0]);
+        int col1 = Integer.parseInt(input[1]);
+
+        int[][] matrix = new int[row1][col1];
+        int sum = 0;
+        for (int row = 0; row < matrix.length; row++) {
+            String[] num = scanner.nextLine().split(", ");
+            for (int col = 0; col < matrix[row].length; col++) {
+                matrix[row][col] = Integer.parseInt(num[col]);
+                sum += matrix[row][col];
+            }
+        }
+        System.out.println(row1);
+        System.out.println(col1);
+        System.out.println(sum);
     }
 }
---
+++ src/JavaAdvanced_Lab/Abstraction/P04_Maximum_Sum_of_2x2_Submatrix.java
@@ -0,0 +1,5 @@
+package JavaAdvanced_Lab.Abstraction;
+
+public class P04_Maximum_Sum_of_2x2_Submatrix {
+    
+}
