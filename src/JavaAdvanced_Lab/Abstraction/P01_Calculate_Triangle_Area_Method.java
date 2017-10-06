67a7d43472fac500097aca50b58752df46edff54
stanislavkisyov <stanislav.kisiov@gmail.com>
2017-10-06 12:54:34 +0300

    Calculate Triangle Area Method

---
+++ src/JavaAdvanced_Lab/Abstraction/P01_Calculate_Triangle_Area_Method.java
@@ -0,0 +1,4 @@
+package JavaAdvanced_Lab.Abstraction;
+
+public class P01_Calculate_Triangle_Area_Method {
+}
---
+++ src/JavaAdvanced_Lab/FUNCTIONAL_PROGRAMMING/P01_Sort_Even_Numbers.java
@@ -0,0 +1,28 @@
+package JavaAdvanced_Lab.FUNCTIONAL_PROGRAMMING;
+
+import java.util.*;
+import java.util.function.Function;
+
+public class P01_Sort_Even_Numbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] input = scanner.nextLine().split(", ");
+
+        List<Integer> numbers = new ArrayList<>();
+        for (String s : input) {
+            numbers.add(Integer.parseInt(s.trim()));
+        }
+
+        numbers.removeIf(x -> x % 2 != 0);
+        for (Integer number : numbers) {
+            System.out.print(number.toString().replaceAll(" ", ","));
+        }
+        System.out.println();
+        numbers.sort((n, m) -> n.compareTo(m));
+        for (Integer number : numbers) {
+            System.out.print(number + ", ");
+        }
+        System.out.println();
+    }
+}
