7b3fcfd73eb13f35810cd4ed9a519a9d2aa7fa23
stanislavkisyov <stanislav.kisiov@gmail.com>
2017-10-09 15:19:47 +0300

    First Name

--- src/JavaAdvanced_Lab/Introducing_Stream_API/P02_Upper_Strings.java
+++ src/JavaAdvanced_Lab/Introducing_Stream_API/P02_Upper_Strings.java
@@ -1,4 +1,19 @@
 package JavaAdvanced_Lab.Introducing_Stream_API;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
+
 public class P02_Upper_Strings {
+    public static void main(String[] args) throws IOException {
+        BufferedReader  bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        List<String> strings = Arrays.asList(bufferedReader.readLine().split("\\s+"));
+
+        strings.stream().map(s -> s.toUpperCase()).forEach(n -> System.out.print(n + " "));
+
+
+    }
 }
---
+++ src/JavaAdvanced_Lab/Introducing_Stream_API/P03_First_Name.java
@@ -0,0 +1,7 @@
+package JavaAdvanced_Lab.Introducing_Stream_API;
+
+public class P03_First_Name {
+    public static void main(String[] args) {
+        
+    }
+}
