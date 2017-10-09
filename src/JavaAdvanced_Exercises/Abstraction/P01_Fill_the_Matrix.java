f127051f614cc435fff49c90c36b42f8537a10d4
stanislavkisyov <stanislav.kisiov@gmail.com>
2017-10-09 11:29:56 +0300

    Fill the Matrix

---
+++ src/JavaAdvanced_Exercises/Abstraction/P01_Fill_the_Matrix.java
@@ -0,0 +1,9 @@
+package JavaAdvanced_Exercises.Abstraction;
+
+import java.util.Scanner;
+
+public class P01_Fill_the_Matrix {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
