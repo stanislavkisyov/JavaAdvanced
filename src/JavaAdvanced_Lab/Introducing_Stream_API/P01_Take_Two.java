761f2049508e3c68896617fad9e8bd1dbbe1bd3c
stanislavkisyov <stanislav.kisiov@gmail.com>
2017-10-09 15:12:07 +0300

    Upper Strings

--- src/JavaAdvanced_Lab/Introducing_Stream_API/P01_Take_Two.java
+++ src/JavaAdvanced_Lab/Introducing_Stream_API/P01_Take_Two.java
@@ -1,4 +1,23 @@
 package JavaAdvanced_Lab.Introducing_Stream_API;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Stream;
+
 public class P01_Take_Two {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<String> tokens = Arrays.asList(scanner.nextLine().split("\\s+"));
+
+        List<Integer> numbers = new ArrayList<>();
+
+        for (String token : tokens) {
+            numbers.add(Integer.valueOf(token));
+        }
+
+        numbers.stream().filter(x -> x >= 10 && x <= 20).distinct().limit(2).forEach(n -> System.out.print(n + " "));
+    }
 }
---
+++ src/JavaAdvanced_Lab/Introducing_Stream_API/P02_Upper_Strings.java
@@ -0,0 +1,4 @@
+package JavaAdvanced_Lab.Introducing_Stream_API;
+
+public class P02_Upper_Strings {
+}
