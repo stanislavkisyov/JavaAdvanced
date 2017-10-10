ac2e479c3966441d220ab4d50df9a8317d9ba948
stanislavkisyov <stanislav.kisiov@gmail.com>
2017-10-10 11:41:22 +0300

    Maximal Sum

--- src/JavaAdvanced_Exercises/Abstraction/P04_2x2_Squares_in_Matrix.java
+++ src/JavaAdvanced_Exercises/Abstraction/P04_2x2_Squares_in_Matrix.java
@@ -1,9 +1,43 @@
 package JavaAdvanced_Exercises.Abstraction;
 
-import java.util.Scanner;
+
+        import java.util.Scanner;
 
 public class P04_2x2_Squares_in_Matrix {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        String[] input = scanner.nextLine().split("\\s+");
+        int sizeRow = Integer.parseInt(input[0]);
+        int sizeCol = Integer.parseInt(input[1]);
+        Character[][] matrix = new Character[sizeRow][sizeCol];
+
+        fillMatrix(scanner, matrix);
+        System.out.println(getEqualsSquareCount(matrix));
+
     }
+
+    private static void fillMatrix(Scanner scanner, Character[][] matrix) {
+        for (int row = 0; row < matrix.length; row++) {
+            String[] reminder = scanner.nextLine().split("\\s+");
+            for (int col = 0; col < matrix[row].length; col++) {
+                matrix[row][col] = reminder[col].charAt(0);
+            }
+        }
+    }
+
+    private static int getEqualsSquareCount(Character[][] matrix) {
+        int count = 0;
+        for (int row = 0; row < matrix.length - 1; row++) {
+            for (int col = 0; col < matrix[row].length - 1; col++) {
+                if (matrix[row][col] == matrix[row][col + 1]
+                        && matrix[row][col] == matrix[row + 1][col]
+                        && matrix[row][col] == matrix[row + 1][col + 1]) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
 }
---
+++ src/JavaAdvanced_Exercises/Abstraction/P05_Maximal_Sum.java
@@ -0,0 +1,9 @@
+package JavaAdvanced_Exercises.Abstraction;
+
+import java.util.Scanner;
+
+public class P05_Maximal_Sum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
