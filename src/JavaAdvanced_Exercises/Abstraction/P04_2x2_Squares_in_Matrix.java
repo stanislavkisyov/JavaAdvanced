8df2cb37ea7515ceaf10c1c1c97e6e6ecdf0165d
stanislavkisyov <stanislav.kisiov@gmail.com>
2017-10-10 11:03:09 +0300

    2x2 Squares in Matrix

---
+++ src/JavaAdvanced_Exercises/Abstraction/P04_2x2_Squares_in_Matrix.java
@@ -0,0 +1,9 @@
+package JavaAdvanced_Exercises.Abstraction;
+
+import java.util.Scanner;
+
+public class P04_2x2_Squares_in_Matrix {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
