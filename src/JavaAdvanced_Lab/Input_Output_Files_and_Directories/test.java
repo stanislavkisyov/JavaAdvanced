fd14df6aa0b495bebb08c6c475022f930dbe922e
stanislavkisyov <stanislav.kisiov@gmail.com>
2017-10-13 11:14:04 +0300

    new

--- src/JavaAdvanced_Lab/Input_Output_Files_and_Directories/test.java
+++ src/JavaAdvanced_Lab/Input_Output_Files_and_Directories/test.java
@@ -1,4 +1,12 @@
 package JavaAdvanced_Lab.Input_Output_Files_and_Directories;
 
+import java.util.Scanner;
+
 public class test {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < ; i++) {
+            
+        }
+    }
 }
