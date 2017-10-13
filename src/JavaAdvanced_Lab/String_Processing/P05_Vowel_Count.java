616cee7a5c28e4ae79ecfde6e5358a917f9dd6b3
stanislavkisyov <stanislav.kisiov@gmail.com>
2017-10-13 13:24:28 +0300

    Vowel Count

--- src/JavaAdvanced_Lab/String_Processing/P05_Vowel_Count.java
+++ src/JavaAdvanced_Lab/String_Processing/P05_Vowel_Count.java
@@ -2,12 +2,9 @@ package JavaAdvanced_Lab.String_Processing;
 
 import java.util.Scanner;
 
-public class P04_Series_of_Letters {
+public class P05_Vowel_Count {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        int state = 0;
-        //Todo:не мога даизмисля решението нещо
+        
     }
-
 }
