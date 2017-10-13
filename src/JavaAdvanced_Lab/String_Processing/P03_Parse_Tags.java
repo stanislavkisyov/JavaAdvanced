f015ffcd3b192a348ed2c0730817a8f17e28f5d9
stanislavkisyov <stanislav.kisiov@gmail.com>
2017-10-13 12:23:31 +0300

    Series of Letters

--- src/JavaAdvanced_Lab/String_Processing/P03_Parse_Tags.java
+++ src/JavaAdvanced_Lab/String_Processing/P03_Parse_Tags.java
@@ -1,4 +1,20 @@
 package JavaAdvanced_Lab.String_Processing;
 
+import java.util.Scanner;
+
 public class P03_Parse_Tags {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        while (input.contains("<upcase>")) {
+            int start = input.indexOf("<upcase>");
+            int end = input.indexOf("</upcase>");
+            String subs= input.substring(start + 8, end);
+
+            input = input.replace("<upcase>" + subs + "</upcase>", subs.toUpperCase());
+        }
+        System.out.println(input);
+    }
 }
---
+++ src/JavaAdvanced_Lab/String_Processing/P04_Series_of_Letters.java
@@ -0,0 +1,9 @@
+package JavaAdvanced_Lab.String_Processing;
+
+import java.util.Scanner;
+
+public class P04_Series_of_Letters {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
