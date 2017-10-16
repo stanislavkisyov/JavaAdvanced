96bbe91b87831242c51db56e2fc1f137664d94fb
stanislavkisyov <stanislav.kisiov@gmail.com>
2017-10-16 14:03:36 +0300

    Match Phone Number

--- src/JavaAdvanced_Exercises/String_Proccesing/P09_Match_Full_Name.java
+++ src/JavaAdvanced_Exercises/String_Proccesing/P09_Match_Full_Name.java
@@ -1,4 +1,19 @@
 package JavaAdvanced_Exercises.String_Proccesing;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class P09_Match_Full_Name {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+
+        String regex = "^([A-Z])([a-z])+ ([A-Z])([a-z])+$";
+        while (!text.equals("end")) {
+            if (Pattern.matches(regex, text)) {
+                System.out.println(text);
+            }
+            text = scanner.nextLine();
+        }
+    }
 }
---
+++ src/JavaAdvanced_Exercises/String_Proccesing/P10_Match_Phone_Number.java
@@ -0,0 +1,10 @@
+package JavaAdvanced_Exercises.String_Proccesing;
+
+import java.util.Scanner;
+
+public class P10_Match_Phone_Number {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+    }
+}
