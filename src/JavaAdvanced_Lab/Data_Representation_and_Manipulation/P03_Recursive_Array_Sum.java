38c270ab25e498b39f89c1f3385e2ff726ba0bd2
stanislavkisyov <stanislav.kisiov@gmail.com>
2017-10-16 23:45:23 +0300

    Recursive Factorial

--- src/JavaAdvanced_Lab/Data_Representation_and_Manipulation/P03_Recursive_Array_Sum.java
+++ src/JavaAdvanced_Lab/Data_Representation_and_Manipulation/P03_Recursive_Array_Sum.java
@@ -1,4 +1,25 @@
 package JavaAdvanced_Lab.Data_Representation_and_Manipulation;
 
+import java.util.Scanner;
+
 public class P03_Recursive_Array_Sum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] input = scanner.nextLine().split("\\s+");
+        int[] arr = new int[input.length];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(input[i]);
+        }
+        int sum = sum(arr, 0);
+        System.out.println(sum);
+    }
+
+    private static int sum(int[] arr, int i) {
+        if (i == arr.length - 1) {
+            return arr[i];
+        }
+        return arr[i] + sum(arr, i + 1);
+    }
+
 }
---
+++ src/JavaAdvanced_Lab/Data_Representation_and_Manipulation/P04_Recursive_Factorial.java
@@ -0,0 +1,4 @@
+package JavaAdvanced_Lab.Data_Representation_and_Manipulation;
+
+public class P04_Recursive_Factorial {
+}
